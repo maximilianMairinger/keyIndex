0769f03e645eaa18045f0e3250c1f34df77d97f5
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-06-30 15:56:09 +0200

    code

--- app/src/keyIndex.ts
+++ app/src/keyIndex.ts
@@ -1,5 +1,12 @@
-
-
-export default function() {
+export default function constructIndex<Pointer, Value>(init: (pointer: Pointer) => Value) {
+  const index: Map<Pointer, Value> = new Map
   
+  return function (pointer: Pointer) {
+    let me = index.get(pointer)
+    if (me === undefined) {
+      me = init(pointer)
+      index.set(pointer, me)
+    }
+    return me
+  }
 }
