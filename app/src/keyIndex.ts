21957493e036bdd9fb79d685a6969c738283741e
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-07-05 11:31:18 +0200

    feature: set

--- app/src/keyIndex.ts
+++ app/src/keyIndex.ts
@@ -1,12 +1,18 @@
 export default function constructIndex<Pointer, Value>(init: (pointer: Pointer) => Value) {
   const index: Map<Pointer, Value> = new Map
   
-  return function (pointer: Pointer) {
-    let me = index.get(pointer)
-    if (me === undefined) {
-      me = init(pointer)
-      index.set(pointer, me)
+  return function (pointer: Pointer, set: Value) {
+    if (set === undefined) {
+      let me = index.get(pointer)
+      if (me === undefined) {
+        me = init(pointer)
+        index.set(pointer, me)
+      }
+      return me
     }
-    return me
+    else {
+      index.set(pointer, set)
+      return set
+    }
   }
 }
