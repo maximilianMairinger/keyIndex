8d599f0e109dcfe37133d11d57eb2e7cdf56c823
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-07-12 15:21:08 +0200

    repl

--- repl/src/repl.ts
+++ repl/src/repl.ts
@@ -1,4 +1,19 @@
-import keyIndex from "./../../app/src/keyIndex"
-//const testElem = document.querySelector("#test")
+import keyIndex, { constructObjectIndex } from "./../../app/src/keyIndex"
 
-keyIndex()
+
+
+let ind = keyIndex(() => keyIndex())
+
+ind("qwe")("eee").key = "val"
+
+console.log(ind())
+
+console.log(ind("qwe")("eee"))
+ind(true)
+
+
+let ind2 = constructObjectIndex(() => constructObjectIndex())
+
+
+ind2("qwe")("ewq").key = "val"
+console.log(ind2("qwe")("ewq"), ind2())
