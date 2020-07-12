49927e2259300da394b168fa9284dd1ecaa0295c
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-07-12 16:18:25 +0200

    rm parse parseInit

--- app/src/keyIndex.ts
+++ app/src/keyIndex.ts
@@ -3,14 +3,9 @@ const indexSymbol = Symbol()
 type Primitive = string | number | symbol
 type GenericObject = {[key in Primitive]: unknown}
 
-function parseInit(init: any) {
-  if (init[indexSymbol]) return () => init()
-  else return init
-}
 
 export function constructIndex<Pointer = unknown, Value = GenericObject>(init: (pointer: Pointer) => Value = () => {return {} as any}) {
   const index: Map<Pointer, Value> = new Map
-  init = parseInit(init)
   
   function ind(pointer: Pointer, set: Value): typeof set
   function ind(pointer: Pointer): Value
@@ -52,7 +47,6 @@ export default constructIndex
 
 export function constructObjectIndex<Pointer extends Primitive = Primitive, Value = GenericObject>(init: (pointer: Pointer) => Value = () => {return {} as any}) {
   const index: any = {}
-  init = parseInit(init)
 
   function ind(pointer: Pointer, set: Value): typeof set
   function ind(pointer: Pointer): Value
