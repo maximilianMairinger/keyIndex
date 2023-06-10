0750011c05933eae714d7c1f0f1de8822345ab9e
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-06-10 20:32:23 +0200

    fix build

--- rollup.node.prod.config.mjs
+++ rollup.node.prod.config.mjs
@@ -5,7 +5,7 @@ import commonMod from "./rollup.node.common.config.mjs"
 export default merge(commonMod, {
   input: 'app/src/keyIndex.ts',
   output: {
-    file: 'dist/cjs/keyIndex.js',
+    file: 'app/dist/cjs/keyIndex.js',
     format: 'cjs'
   },
 })
